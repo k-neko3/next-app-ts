fcc74ed505c39272df1d63b442e6c311e74ae3f2
k-neko3 <84276658+k-neko3@users.noreply.github.com>
2022-11-14 20:28:00 +0900

    update: バージョンアップ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
